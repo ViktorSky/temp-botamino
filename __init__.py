8c8cf14cdfc21d9869aaafe470c01105fc20fdfd
Viktor <99266266+ViktorSky@users.noreply.github.com>
2022-09-11 17:22:08 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from ._command import Command
-from ._botamino import BotAmino
-from ._bot import Bot
-from ._timeout import TimeOut
-from ._bannedwords import BannedWords
-
-__version__ = "0.0.0.0"
